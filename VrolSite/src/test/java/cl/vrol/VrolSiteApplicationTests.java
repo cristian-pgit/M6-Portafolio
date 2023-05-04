7860afbb7baf4d4baf2325b9dd48c4f823127575
Cristian Carrillo <naitsircl@gmail.com>
2023-05-04 13:41:21 -0400

    springboot

---
+++ VrolSite/src/test/java/cl/vrol/VrolSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.vrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrolSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
